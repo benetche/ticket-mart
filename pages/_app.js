0a79002734b5ae7617f39996b98005610df87b61
Vítor Beneti Martins <vitor.beneti@gmail.com>
2022-06-15 12:11:55 -0300

    finished: login page

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,15 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import Header from '../components/Header'
+import {useRouter} from 'next/router'
 
 function MyApp({ Component, pageProps }) {
-
+  if(useRouter().pathname === '/login'){
+    return(<Component {...pageProps} />)
+  }
   return(
     <div>
-      {/* Always renders this Header component */}
-      {/* <Header/>  */}
+      <Header/> 
         <Component {...pageProps} />
     </div>
   
--- pages/login.js
+++ pages/login.js
@@ -2,14 +2,21 @@
 import Head from "next/head"
 import LoginContainer from "../components/LoginContainer"
 import styles from '../styles/Login.module.css'
+import Logo from '../assets/logoAlt.png'
+import Image from "next/image"
 export default function Login(){
 
     return(
-        <div className={styles.main}>
+        <div style={{margin: "auto"}}>
             <Head>
                 <title>Log in - tik.me</title>
             </Head>
-            <div>
+            <div className={styles.main}>
+                <div className={styles.imageContainer}>
+                <Image src={Logo} alt='logo'
+                    width={300} height={100} className={styles.logo} style={{margin: "auto"}} />
+                <p style={{color: "white", fontSize: "2em"}}>Enjoy your time.</p>
+                </div>
                 <LoginContainer/>
             </div>
         </div>

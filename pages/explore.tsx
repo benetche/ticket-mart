092cf0c3cef254f249762ed4be99aa23b3267e72
Beneti <vitor.beneti@gmail.com>
2022-06-20 17:04:10 -0300

    finished: search bar for explore events

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,5 +1,30 @@
-import { Grid, TextField, Button } from '@mui/material';
+import {
+  Grid,
+  TextField,
+  Button,
+  InputAdornment,
+  IconButton,
+} from '@mui/material';
+import { Container } from '@mui/system';
 import EventCard from '../src/EventCard';
+import { SearchOutlined } from '@mui/icons-material';
+
+const EventContainer = () => {
+  return (
+    <Grid container direction="row">
+      <Grid item sm={4} xs={12}>
+        <EventCard />
+      </Grid>
+      <Grid item sm={4} xs={12}>
+        <EventCard />
+      </Grid>
+      <Grid item sm={4} xs={12}>
+        <EventCard />
+      </Grid>
+    </Grid>
+  );
+};
+
 export default function Explore() {
   return (
     <Grid direction="row" container p={4} sm={12}>
@@ -9,26 +34,22 @@ export default function Explore() {
             variant="outlined"
             label="Buscar Evento"
             fullWidth
-            size="small"
+            size="medium"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="start">
+                  <IconButton>
+                    <SearchOutlined />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
         </Grid>
       </Grid>
-      <Grid direction="column" item sm={4} xs>
-        <Button variant="contained" color="success">
-          Buscar
-        </Button>
-      </Grid>
-      <Grid container direction="row">
-        <Grid item sm={4} xs={12}>
-          <EventCard />
-        </Grid>
-        <Grid item sm={4} xs={12}>
-          <EventCard />
-        </Grid>
-        <Grid item sm={4} xs={12}>
-          <EventCard />
-        </Grid>
-      </Grid>
+      <EventContainer />
+      <EventContainer />
+      <EventContainer />
     </Grid>
   );
 }

fdc7046ab47d4a3bba5120190355b22e5708d311
Vítor Beneti Martins <vitor.beneti@gmail.com>
2022-06-27 16:41:58 -0300

    added: publish event request on json server

--- pages/publish.tsx
+++ pages/publish.tsx
@@ -6,22 +6,76 @@ import {
   Button,
   Paper,
 } from '@mui/material';
-
+import { useState } from 'react';
+import { json } from 'stream/consumers';
 const PublishForm = () => {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [tel, setTel] = useState('');
+  const [desc, setDesc] = useState('');
+
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        fetch('http://localhost:3000/requests', {
+          method: 'POST',
+          body: JSON.stringify({
+            name: `${name}`,
+            surname: `${surname}`,
+            email: `${email}`,
+            tel: `${tel}`,
+            description: `${desc}`,
+          }),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' },
+        }).then(() => console.log('Enviado com sucesso!'));
+      }}
+    >
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
-          <TextField label="Nome" required fullWidth type="text" />
+          <TextField
+            label="Nome"
+            required
+            fullWidth
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField label="Sobrenome" fullWidth required type="text" />
+          <TextField
+            label="Sobrenome"
+            fullWidth
+            required
+            type="text"
+            onChange={(e) => {
+              setSurname(e.target.value);
+            }}
+          />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField label="Email" fullWidth required type="email" />
+          <TextField
+            label="Email"
+            fullWidth
+            required
+            type="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField label="Telefone" fullWidth required type="tel" />
+          <TextField
+            label="Telefone"
+            fullWidth
+            required
+            type="tel"
+            onChange={(e) => {
+              setTel(e.target.value);
+            }}
+          />
         </Grid>
         <Grid item xs={12} sm={12}>
           <TextField
@@ -30,6 +84,9 @@ const PublishForm = () => {
             required
             type="text"
             multiline
+            onChange={(e) => {
+              setDesc(e.target.value);
+            }}
           />
           <Grid
             direction="column"
